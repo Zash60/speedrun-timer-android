a55637b59bf62a913209cddba90195da4274e8ba
zash60 <xath06@gmail.com>
2026-04-13 12:47:50 +0000

    Update Settings and Help activities to reach the notch with custom Toolbars

--- app/src/main/kotlin/il/ronmad/speedruntimer/activities/HelpActivity.kt
+++ app/src/main/kotlin/il/ronmad/speedruntimer/activities/HelpActivity.kt
@@ -2,6 +2,10 @@ package il.ronmad.speedruntimer.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.mukesh.MarkdownView
 import il.ronmad.speedruntimer.R
 
@@ -10,6 +14,17 @@ class HelpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_help)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        // Fullscreen: hide status bar and navigation bar
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, window.decorView).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         val markdownView = findViewById<MarkdownView>(R.id.markdownView)
         markdownView.loadMarkdownFromAssets("help.md")
--- app/src/main/kotlin/il/ronmad/speedruntimer/activities/SettingsActivity.kt
+++ app/src/main/kotlin/il/ronmad/speedruntimer/activities/SettingsActivity.kt
@@ -2,15 +2,21 @@ package il.ronmad.speedruntimer.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import il.ronmad.speedruntimer.R
 import il.ronmad.speedruntimer.fragments.SettingsFragment
 
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
         // Fullscreen: hide status bar and navigation bar
         WindowCompat.setDecorFitsSystemWindows(window, false)
@@ -21,7 +27,7 @@ class SettingsActivity : AppCompatActivity() {
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportFragmentManager.beginTransaction()
-                .replace(android.R.id.content, SettingsFragment())
+                .replace(R.id.fragment_container, SettingsFragment())
                 .commit()
     }
 }
